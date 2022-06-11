8ab13bfeeeef8a65e79d158b9765bd8bd021b59c
IrynaShvets <irinashwets@ukr.net>
2022-06-11 16:11:54 +0300

    Updated react-app

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const cors = require('cors');
 const app = express();
 
@@ -13,3 +13,4 @@ app.use('/login', (req, res) => {
 app.listen(8080, () =>
   console.log('API is running on http://localhost:8080/login'),
 );
+ */
